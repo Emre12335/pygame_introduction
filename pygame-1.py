11af72a2d6cf1736078aa52825ff6399db300a69
Emre Ozdemir <emrozd2000@gmail.com>
2021-08-30 21:30:21 +0300

    pygame'deki while loop açıklandı

--- pygame-1.py
+++ pygame-1.py
@@ -7,3 +7,8 @@ pygame.init() # Bu pygame in kurulmasını sağlıyor.Pygame ile çalışmaya ba
 screen = pygame.display.set_mode((800,400))#pygame.display module ü ekranla igili işlemlerde kullanıyoruz.set_mode en az 1 parametre almak
 # zorunda ve bu parametre tuple halinde olmak zorunda height ve width olmak üzere 2 tane değer giriyoruz.
 
+# tkinter daki gibi pygame de mainloop yok çünkü tkinter dakinden farklı olarak resimler arası geçişler olacak oyüzden.
+# oynattığımız pencerenin açık kalması için while loop açıyoruz.
+
+while True:
+    pass
