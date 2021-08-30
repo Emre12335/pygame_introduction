381a3e7d7e3eedadbdb093b60bb6a90155b76cf2
Emre Ozdemir <emrozd2000@gmail.com>
2021-08-30 22:21:41 +0300

    pygame.update.set_caption() başlık değiştirme

--- pygame-1.py
+++ pygame-1.py
@@ -9,6 +9,8 @@ screen = pygame.display.set_mode(
     (800, 400))  # pygame.display module ü ekranla igili işlemlerde kullanıyoruz.set_mode en az 1 parametre almak
 # zorunda ve bu parametre tuple halinde olmak zorunda height ve width olmak üzere 2 tane değer giriyoruz.
 
+pygame.display.set_caption("OYUN1")#oyunun ismi bu şekilde değiştirilebilir.
+
 # tkinter daki gibi pygame de mainloop yok çünkü tkinter dakinden farklı olarak resimler arası geçişler olacak oyüzden.
 # oynattığımız pencerenin açık kalması için while loop açıyoruz.
 
