9f8e8a80129641105375d42c459a366c7787cc63
Emre Ozdemir <emrozd2000@gmail.com>
2021-08-31 13:34:12 +0300

    image surface nasıl yapılılıp blit edilir gösterildi

--- pygame-2.py
+++ pygame-2.py
@@ -14,19 +14,16 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")
-test_surface = pygame.Surface((100,200))# surface with plain color,Bu objenin tuple şeklinde bir parametreye ihtiyacı var.tuple ın içine width ve height gireceğiz.
-test_surface.fill("Red") # yüzeyin rengini kırmızı yapıyor
-test_surface.fill((0,100,100)) #bu şekilde de yazılabilir.
-# not normalde fill in içine 3 elemanlı tuple girerek de rengi belirleyebiliriz onun listesi internette var.
+image_sky_surface = pygame.image.load("graphics/Sky.png")
+image_ground_surface = pygame.image.load("graphics/ground.png")
 clock_object = pygame.time.Clock()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(test_surface,(200,100)) #screen.blit in içine oynatmak istediğimiz surface ı ve poziyonunun tuple şeklinde giriyoruz.
-    # bu yüzeyin ekranda gözükmesini sağlıyor.#fakat böyle bırakırsan birşey gözükmez çünkü rengi siyah olduğu için gözükmez
-    # 0,0 ekranın sol en üst köşesi birinic değer widdth artırıldığında ekranın sol tarafından uzaklaşıp sağ tarafına yaklaşır.
-    # ikinici değer height artırılısa ekranın alt tarafına yaklaşır.
+    screen.blit(image_sky_surface,(0,0))#blit etme sırası önemli çünkü eğer ground u önce bilit etseydim ve büyüklüğü daha yüksek
+    # olsaydı gözükmezdi blitte gelen yüzeyler üst üste binip gözükmeyebilir.
+    screen.blit(image_ground_surface,(0,300))
     pygame.display.update()
     clock_object.tick(60)
