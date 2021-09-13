77f27a10acb934d6696a736bf1596010d8bf7590
Emre Ozdemir <emrozd2000@gmail.com>
2021-09-13 20:26:52 +0300

    pygame introduction is added.

---
+++ pygame-!introduction.py
@@ -0,0 +1,36 @@
+# Zero to Hero pygame
+
+# Video oyunu nedir?
+
+# Bunun cevabını vermek için öncelikle filmlerle kıyas yapmalıyız.Filmlerde gördüğümüz şey aslında saniye başına geçen 24 tane resimdir.
+# Genelde bu resimler önceden kayıtlıdır ve sırayla oynatılması sonucu filmler oluşur.kulandığın resmin kalitesine göre animasyon,gerçekçekim,
+# ... gibi farklı tipte resimler kullanılabilir.
+# Video oyunlarında ise resimler  kayıtlı değildir.Kullanıcı için özel olarak saniyede yada belirli bir sürede bilgisayar tarafından yaratılır.
+# Diğer bir fark isevideo oyunları kullanıcıdan gelecek olan girdi ve çıktılara göre formatı belirler örneğin oyuncu seviyeyi geçemediyse.
+# Oyun baştan başlar ...
+# Yada örneğin kullanıcı yukarı tuşuna bastığı zaman bilgisayar karakterin zıpladığı resmi gösterir
+
+
+# Pygame ne yapar?
+
+# pygame bizim ekranda resim çizmemizi sağlar ve o resimleri kullanıcıya göndermemizi sağlar.
+# yani pygame bir oyun kurma amaçlı yapılsa bile aslında bir film bile oynatabilirsin.
+# resim çizmek zor olduğu için pygame sayesnde resimleri oynatabiliriz.
+# pygame kullanıcı inputları için elverişlidir. Ve inputları sürekli kontrol eder.
+# python dahazır input alıcı varken niye pygame ise input() fonksiyonu oyunun durmasına yol açar ve reaksiyonu azaltır.Bu yüzden pygame tercih edilir.
+
+
+# Pygame ile modern ve grafik kalitesi yüksek oyunlar yapılamaz.
+
+# red dead redemption gta call of duty gibi oyunlar yapmak istiyorsan untiy unreal engine gibi oyun motorları kullanmalısın yani pygame kullnrak.
+# oyun geliştirici olamazsın.
+
+
+# Peki niye pygame öğrenmeliyiz?
+
+# pygame öğrenmek iyi bir programlama alt yapısı ve bilgisi oluşturur ve seni daha iyi bir programcı yapar.
+# çünkü unity unreal engine gibi platformlar kullanıcılara çalışmaları için yardımcı ögeler sağlar ve kodlamadan daha çok
+# yapılan işin görüntüsü ile ilgilenir.
+
+# Oyun geliştiriciler için yeni oyun motorları öğrenmek kolaydır.Fakat bunu sadece programlama bilgisi iyi olan oyun geliştiticileri yapabilir.
+
